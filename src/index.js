ff7f0740d198798586ec0df9bc9ce687778eb310
xiaoshen110 <xiaoshen110>
2019-09-26 14:22:45 +0800

    'react-test'

--- src/index.js
+++ src/index.js
@@ -4,6 +4,26 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// import router from './routers'
+
+// import modelIndex from './models'
+
+// 1.引入dva
+// import Dva from 'dva'
+
+// // 2 创建dva实例
+// let app = new Dva()
+// // 3 配置路由
+// app.router(router)
+// //  4 app.use 安装插件
+// // app.use()
+// //  5.注册模块app.model({})
+// app.model(modelIndex)
+// //  5.5 卸载
+// // app.unmodel()
+// //  6. 启动app
+// app.start('#root')
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
